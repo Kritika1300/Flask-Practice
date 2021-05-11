fcfcd358d19da3dd826c9873c889f941c0fd15eb
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-11 17:50:24 +0530

    Templates

--- Practice/app.py
+++ Practice/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "<h1>hello!</h1>"
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 @app.route('/about')
 def about():

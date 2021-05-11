73919381bfa9b08e5f73d3b7ca1058a5d9e0232c
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-11 21:29:39 +0530

    Adding html forms to the flask application

--- Practice/app.py
+++ Practice/app.py
@@ -1,18 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1>hello!</h1>"
-
-@app.route('/index')
 def index():
     return render_template("index.html")
 
-@app.route('/contact/<user>')
-def contact(user):
-    return render_template("contact.html", user = user)
+@app.route('/sign_form')
+def sign_form():
+    return render_template("signup.html")
 
+@app.route('/thank_you')
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template("thankyou.html",first = first, last= last)
+
+@app.errorhandler(404)
+def error(e):
+    return render_template("404.html"),404
 
 app.run(port=5000,debug=True)

4faf41626c13deb25ff6d0d64a5b7af4a0a49101
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-11 20:06:11 +0530

    Template Inheritance

--- Practice/app.py
+++ Practice/app.py
@@ -8,20 +8,11 @@ def home():
 
 @app.route('/index')
 def index():
-    my_list = [1,2,3,4,5]
-    return render_template("index.html",my_list = my_list)
+    return render_template("index.html")
 
-# @app.route('/about')
-# def about():
-#     return "<h1>This is the about us page!</h1>"
+@app.route('/contact/<user>')
+def contact(user):
+    return render_template("contact.html", user = user)
 
-# @app.route('/some_page/<name>')
-# def user(name):
-#     l = len(name)
-#     if(name[l-1] == 'y'):
-#         name = name[0:l-1] + 'utiful'
-#     else:
-#         name = name + 'y'
-#     return 'Hey {} !'.format(name.upper())
 
 app.run(port=5000,debug=True)

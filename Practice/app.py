4a1207a6abc63d8a96764a0a126245952d3ed649
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-11 15:22:05 +0530

    Static routing

--- Practice/app.py
+++ Practice/app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "<h1>hello!</h1>"
 
+@app.route('/about')
+def about():
+    return "<h1>This is the about us page!</h1>"
 
 app.run(port = 5000,debug = True)

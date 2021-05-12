cc2597c507f32267ee0cecb25debffbdf188db76
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-12 21:32:36 +0530

    Read operation in db

--- Practice/crud.py
+++ Practice/crud.py
@@ -1,3 +1,4 @@
+from os import name
 from app import db,Puppy
 
 #create
@@ -6,5 +7,16 @@ db.session.add(my_puppy)
 db.session.commit()
 
 #read
+all_puppies = Puppy.query.all() #list of all Puppy(class) objects
+print(all_puppies)
+
+#select by id
+puppy_one = Puppy.query.get(1)
+print(puppy_one.name)
+
+#filter (eg. to get puppies with name sam)
+puppy_sam = Puppy.query.filter_by(name = "sam")
+print(puppy_sam.all()) #prints result of filter method as per __repr__ method
+
 
 

8a211d65711d2af3ac9730424843e76591e28033
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-13 15:03:27 +0530

    Puppy Adoption Website

--- Practice/forms.py
+++ Practice/forms.py
@@ -8,3 +8,9 @@ class AddForm(FlaskForm):
 class DelForm(FlaskForm):
     id = IntegerField('Id of the puppy you want to remove :')
     submit = SubmitField('Submit')
+
+class AddOwnerForm(FlaskForm):
+    name = StringField('Name of the Owner :')
+    pup_id = IntegerField('Id of the Puppy :')
+    submit = SubmitField('Add Owner')
+

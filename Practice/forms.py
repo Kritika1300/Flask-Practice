fe2ba75b46e1c998637913ec2c63c31ca7478d1b
Kritika1300 <62663065+Kritika1300@users.noreply.github.com>
2021-05-14 14:12:52 +0530

    User Authentication + Login Project

--- Practice/forms.py
+++ Practice/forms.py
@@ -1,16 +1,26 @@
+from myProject.models import User
 from flask_wtf import FlaskForm
-from wtforms import StringField,IntegerField,SubmitField
+from flask_wtf.recaptcha import validators
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,EqualTo,Email
+from  wtforms import ValidationError
 
-class AddForm(FlaskForm):
-    name = StringField('Name of the puppy :')
-    submit = SubmitField('Submit')
+class LoginForm(FlaskForm):
+     email = StringField('Email',validators = [DataRequired(),Email()])
+     password = PasswordField('Password',validators = [DataRequired()])
+     submit = SubmitField('Log In')
 
-class DelForm(FlaskForm):
-    id = IntegerField('Id of the puppy you want to remove :')
-    submit = SubmitField('Submit')
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',validators = [DataRequired(),Email()])
+    username = StringField('Username',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(),EqualTo('pass_confirm',message= "Passwords must match !")])
+    pass_confirm = PasswordField('Confirm Password',validators = [DataRequired()])
+    submit = SubmitField('Register')
 
-class AddOwnerForm(FlaskForm):
-    name = StringField('Name of the Owner :')
-    pup_id = IntegerField('Id of the Puppy :')
-    submit = SubmitField('Add Owner')
-
+def check_email(self,field):
+    if User.query.filter_by(email = field.data).first():
+        raise ValidationError('Your email has already been registered !')
+    
+def check_username(self,field):
+    if User.query.filter_by(username = field.data).first():
+        raise ValidationError('This username is already taken !')

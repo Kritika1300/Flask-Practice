7d09f6edabcaca577de8b5edf9acb1d7106b90c6
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-14 14:03:42 +0530

    User authentication + Login (myProject)

---
+++ Practice/myProject/forms.py
@@ -0,0 +1,26 @@
+from myProject.models import User
+from flask_wtf import FlaskForm
+from flask_wtf.recaptcha import validators
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,EqualTo,Email
+from  wtforms import ValidationError
+
+class LoginForm(FlaskForm):
+     email = StringField('Email',validators = [DataRequired(),Email()])
+     password = PasswordField('Password',validators = [DataRequired()])
+     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',validators = [DataRequired(),Email()])
+    username = StringField('Username',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(),EqualTo('pass_confirm',message= "Passwords must match !")])
+    pass_confirm = PasswordField('Confirm Password',validators = [DataRequired()])
+    submit = SubmitField('Register')
+
+def check_email(self,field):
+    if User.query.filter_by(email = field.data).first():
+        raise ValidationError('Your email has already been registered !')
+    
+def check_username(self,field):
+    if User.query.filter_by(username = field.data).first():
+        raise ValidationError('This username is already taken !')

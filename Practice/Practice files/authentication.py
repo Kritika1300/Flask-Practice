cf057d0afd7243154d541262ce093d31a50b0439
Kritika <kaur.kritika.kritika@gmail.com>
2021-05-17 12:42:30 +0530

    Basic Rest Api Example

---
+++ Practice/Practice files/authentication.py
@@ -0,0 +1,15 @@
+# import bcrypt
+# from flask_bcrypt import Bcrypt
+
+# bcrypt = Bcrypt()
+
+# password = 'mysecretpassword'
+
+# #Hashing the user password
+# hashed_password = bcrypt.generate_password_hash(password=password)
+# print(hashed_password)
+
+
+# #Checking hashpassword against other passwords
+# check = bcrypt.check_password_hash(hashed_password,'mysecretpassword')
+# print(check)
